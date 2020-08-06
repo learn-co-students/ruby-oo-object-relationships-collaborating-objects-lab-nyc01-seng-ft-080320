0a82c40315885e7ce36ccf7f28b668fdea937d31
royFloresNyc <royfloresnyc@gmail.com>
2020-08-05 23:00:13 -0400

    Implement all instance and class methods in Artist

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,18 +16,23 @@ class Artist
   end 
 
   def songs
-
+    Song.all.filter { |song| song.artist == self }
   end 
 
   def add_song(song)
     song.artist = self 
   end 
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(artist_name)
+    if self.all.find { |artist| artist.name == artist_name }.nil?
+      Artist.new(artist_name)
+    else 
+      self.all.find { |artist| artist.name == artist_name }
+    end 
   end 
 
   def print_songs
-  
+    self.songs.each { |song| puts song.name }
   end 
 
 end 

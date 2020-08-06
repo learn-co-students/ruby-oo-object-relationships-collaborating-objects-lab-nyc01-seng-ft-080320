c99d566137724e7f8d12ec4290bbf36a985309ae
AmSmo <adam.smolenski@gmail.com>
2020-08-05 21:37:46 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select { |song| song.artist == self}
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(name)
+        found = self.all.find { |artist| artist.name == name} 
+        if found
+            return found
+        else
+            return Artist.new(name)
+        end
+    end
+
+    def print_songs
+        songs.each { |song| puts song.name }
+    end
+end

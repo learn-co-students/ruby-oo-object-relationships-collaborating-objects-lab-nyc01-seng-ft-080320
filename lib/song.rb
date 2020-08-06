5671be8afc343f474fa6a0bc746c5ace5585f006
royFloresNyc <royfloresnyc@gmail.com>
2020-08-05 23:04:36 -0400

    Song class sans Song.new_by_filename

--- lib/song.rb
+++ lib/song.rb
@@ -11,4 +11,16 @@ class Song
     @@all << self
   end 
 
+  def self.all
+    @@all
+  end 
+
+  def self.new_by_filename(some_filename)
+  
+  end 
+
+  def artist_name=(artist_name)
+    @artist = Artist.find_or_create_by_name(artist_name)
+  end 
+
 end 
